b159e048f1a7e835cb8618efbae774208d24f112
傑尼JENNE\admin <Jenne14294@gmail.com>
2024-12-19 18:36:17 +0800

    fixed app.py

--- Food_Pangolin/app.py
+++ Food_Pangolin/app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request, session, redirect
 from functools import wraps
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from modules import init
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/")
 app.config['SECRET_KEY'] = '123TyU%^&'
 
-# 數據庫鏈接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'  # 記得幫我更換數據庫的URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# # 數據庫鏈接
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'  # 記得幫我更換數據庫的URL
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-# 模型
-class Menu(db.Model):
-    __tablename__ = 'menu'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+# # 模型
+# class Menu(db.Model):
+#     __tablename__ = 'menu'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.Float, nullable=False)
 
-# 獲取菜單數據
-def get_vendor_menu():
-    menu_items = Menu.query.all()
-    return [{'name': item.name, 'price': item.price} for item in menu_items]
+# # 獲取菜單數據
+# def get_vendor_menu():
+#     menu_items = Menu.query.all()
+#     return [{'name': item.name, 'price': item.price} for item in menu_items]
 
 # 登錄驗證
 def login_required(f):
@@ -36,13 +36,15 @@ def login_required(f):
 @app.route("/", methods=['GET', 'POST']) 
 @login_required
 def homepage():
-    # 登錄後進菜單
-    return redirect("/menu")
+    # 獲取數據中菜單數據
+    menu = [{}]
+    myname = session.get('loginID', 'User')  # 獲取用戶登錄名稱
+    return render_template('/client/menu.html', menu=menu, myname=myname)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == "GET":
-        return render_template("register.html")
+        return redirect("register.html")
     
     form = request.form
     name = form['NAME']
@@ -61,7 +63,7 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return redirect("/login.html")
     
     form = request.form
     mail = form['MAIL']
@@ -69,20 +71,10 @@ def login():
     
     # 檢查用戶憑證
     user_from_mail = init.check_account(mail, pwd)
+    print(user_from_mail)
     if user_from_mail == []:
-        return redirect("/login.html")
+        return redirect("/login")
     
     # 登錄成功並保存用戶信息 session
     session['loginID'] = mail
     return redirect("/")
-
-@app.route('/menu')
-@login_required
-def menu():
-    # 獲取數據中菜單數據
-    menu = get_vendor_menu()
-    myname = session.get('loginID', 'User')  # 獲取用戶登錄名稱
-    return render_template('menu.html', menu=menu, myname=myname)
-
-if __name__ == '__main__':
-    app.run(debug=True)
