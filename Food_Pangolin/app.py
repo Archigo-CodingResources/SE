f846b58547c260f7d05f34a5fd1a5cd075d4920a
LAPTOP-DNENIIPQ\Archigo <archigo.hi2@gmail.com>
2024-12-21 22:01:43 +0800

    no message

--- Food_Pangolin/app.py
+++ Food_Pangolin/app.py
@@ -229,6 +229,15 @@ def logout():
     session.clear()
     return redirect("/")
 
+
+# @app.route("/submit_feedback", methods=["POST"])
+# def submit_feedback_form():
+#     order_id = request.form['order_id']
+#     rating = int(request.form['rating'])
+#     feedback = request.form['feedback']
+#     client.submit_feedback(order_id, rating, feedback)
+#     return redirect(url_for('restaurant'))
+
 # @app.route("/restaurant")
 # @login_required
 # def restaurant():
